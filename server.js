523d22dc532d10f34c61e924e89ddab435fe4acb
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-01-14 16:45:58 +0700

    release v2

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-var fs = require('fs');
-var https = require('https');
-var privateKey  = fs.readFileSync('./sslcert/key.pem', 'utf8');
-var certificate = fs.readFileSync('./sslcert/server.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 80;
+
 app.use(express.static('dist'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// your express configuration here
 
-var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(443);
-
-console.log("Listening on port 443...");
